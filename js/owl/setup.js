8c6aaa4a5f8f79ef3f51643742cdd5c3e5dd5ec0
mnsJr <98279032+mnsjr@users.noreply.github.com>
2022-02-01 04:35:52 -0300

    Projeto Clone Netflix Iluminar Fotografia

---
+++ js/owl/setup.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        },
+        1400:{
+            items:8
+        }
+    }
+})
